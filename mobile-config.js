ebb00727e777958685a38ce49b2f3a173a6667a4
guro3210 <guido-r@hotmail.de>
2015-05-12 23:01:25 +0200

    Fehlerhafter Kommit angepasst

--- mobile-config.js
+++ mobile-config.js
@@ -1,12 +1,15 @@
 App.info({
-  description: 'The Instant Answering App',
+  name: 'Sighty',
+  description: 'The Instant Answering App', /* app description */
   version: '0.0.1'
 });
 
 App.icons({
+  'android_ldpi': 'resources/icons/icon-ldpi.png', /* specifies android icons */
   'android_mdpi': 'resources/icons/icon-mdpi.png',
   'android_hdpi': 'resources/icons/icon-hdpi.png',
   'android_xhdpi': 'resources/icons/icon-xhdpi.png',
+  'iphone': 'resources/icons/icon-60.png', /* specifies iOS icons */
   'iphone_2x': 'resources/icons/icon-60@2x.png',
   'ipad': 'resources/icons/icon-72.png',
   'ipad_2x': 'resources/icons/icon-72@2x.png'
