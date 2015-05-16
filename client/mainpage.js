5135e5f2fbbce6955d517db212cac7bc84314084
lanparty41 <lantuilmenau@gmail.com>
2015-05-16 20:47:58 +0200

    relative zeit mit moment

--- client/mainpage.js
+++ client/mainpage.js
@@ -1,7 +1,22 @@
 Template.mainpage.helpers({
-  questions: [
-    { text: "Wo wohnt Totleben?", createdAt: new Date(), pictureUrl: "http://blog.ivanstanev.com/wp-content/gallery/bitte-bei-totleben-klingeln/Bitte_bei_TOTLEBEN_klingeln.jpg"},
-    { text: "Ist der cute?", createdAt: new Date(), pictureUrl: "http://animalfair.com/wp-content/uploads/2012/08/Funny-dog-hair-do-image.png"},
-    { text: "Was ist das?", createdAt: new Date(),pictureUrl: "http://www.boredomsquare.com/wp-content/uploads/2015/01/via-buymelaughs1.jpg"}
-  ]
+  questions: [{
+    text: "Wo wohnt Totleben?",
+    createdAt: new Date(),
+    pictureUrl: "http://blog.ivanstanev.com/wp-content/gallery/bitte-bei-totleben-klingeln/Bitte_bei_TOTLEBEN_klingeln.jpg"
+  }, {
+    text: "Ist der cute?",
+    createdAt: new Date(),
+    pictureUrl: "http://animalfair.com/wp-content/uploads/2012/08/Funny-dog-hair-do-image.png"
+  }, {
+    text: "Was ist das?",
+    createdAt: new Date(),
+    pictureUrl: "http://www.boredomsquare.com/wp-content/uploads/2015/01/via-buymelaughs1.jpg"
+  }],
+});
+
+Template.question.helpers({
+  fromNow: function(date) {
+    if (date)
+      return moment(date).fromNow(true);
+  }
 });
