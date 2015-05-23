dcf48ae1dae0f3402705e903f3ecea3c77fd1f6a
alfa8403 <Alex@Alex-MBP.local>
2015-05-23 19:32:00 +0200

    new Design

--- server/social-config.js
+++ server/social-config.js
@@ -1,15 +1,14 @@
-/*
-ServiceConfiguration.configurations.remove({ // removes current facebook login configuration
+ServiceConfiguration.configurations.remove({
     service: 'facebook'
 });
-
-ServiceConfiguration.configurations.insert({ // creates a new configuration for login with facebook
+ 
+ServiceConfiguration.configurations.insert({
     service: 'facebook',
-    appId: '1176364205723187', // authentification information for login with oAuth
+    appId: '1176364205723187',
     secret: '8a326eba318f74cfca338827b103ef8c'
 });
 
-ServiceConfiguration.configurations.remove({ // see above
+ServiceConfiguration.configurations.remove({
   service: "twitter"
 });
 ServiceConfiguration.configurations.insert({
@@ -17,4 +16,3 @@ ServiceConfiguration.configurations.insert({
   consumerKey: "HMBGcyJ3FZ0KhZsEgHH9cB3tW",
   secret: "qSFkZJdfMX4y9jQCMGSWvWmXuKH7XwG94ppbkrzQfwwQyb3anS"
 });
-*/
