e6ddeab183d2b9a2c37e790a50739a387855e886
alfa8403 <Alex@q084.fem.tu-ilmenau.de>
2015-05-28 23:07:19 +0200

    random funny usernames (by Alex & Felix)

--- client/views/login/login.js
+++ client/views/login/login.js
@@ -25,10 +25,17 @@ Template.login.events({
 	"click #btn_register_register":function(event, template){
 		emailVar = template.find("#email_register").value;
 		passwordVar = template.find("#password_register").value;
+		var usernames1 = ["grinsender", "kreischender", "schöner", "wachsamer", "großer", "weißer", "starker", "mutiger", "tanzender", "blauer", "roter", "listiger", "kräftiger", "furchtloser", "silberner", "anmutiger", "mächtiger", "mystischer", "potenter", "wohlriechender", "launischer", "lebensfroher", "geheimnisvoller", "grimmiger", "optimistischer", "pessimistischer", "respektabler", "impulsiver", "dominanter", "romantischer", "kaltblütiger", "schüchterner", "japsender", "tauchender", "rülpsender", "pfeifender", "sturer", "kitzliger", "flinker", "kleiner", "verrückter", "abgedrehter", "hyperaktiver", "trotzender", "cooler", "krasser", "schlauer"];
+		var usernames2 = ["Hummer", "Wolf", "Büffel", "Wurm", "Adler", "Stier", "Hund", "Blutegel", "Hirsch", "Pinguin", "Papagei", "Hamster", "Hase", "Nacktmul", "Zander", "Dachs", "Igel", "Blobfisch", "Flamingo", "Leopard", "Maulwurf", "Elch", "Steinbock", "Esel", "Blauwal", "Gekko", "Käfer", "Salamander", "Grizzly", "Puma", "Gepard", "Elefant", "Luchs", "Uhu", "Waschbär", "Pfau", "Fasan", "Schwan", "Rochen", "Affe", "Schimpanse", "Orang-Utan", "Kabeljau", "Aal", "Thunfisch", "Schmetterling", "Eisbär", "Tiger", "Hai", "Panda", "Mops", "Pavian", "Orka", "Dackel", "Gorilla", "Storch", "Frosch", "Krake", "Pelikan", "Koala"];
+		var profilbilder = ["/profilbilder/profilbild1.jpg", "/profilbilder/profilbild2.jpg", "/profilbilder/profilbild3.jpg", "/profilbilder/profilbild4.jpg", "/profilbilder/profilbild5.jpg", "/profilbilder/profilbild6.jpg", "/profilbilder/profilbild7.jpg", "/profilbilder/profilbild8.jpg", "/profilbilder/profilbild9.jpg", "/profilbilder/profilbild10.jpg", "/profilbilder/profilbild11.jpg", "/profilbilder/profilbild12.jpg", "/profilbilder/profilbild13.jpg", "/profilbilder/profilbild14.jpg", "/profilbilder/profilbild15.jpg", "/profilbilder/profilbild16.jpg", "/profilbilder/profilbild17.jpg", "/profilbilder/profilbild18.jpg", "/profilbilder/profilbild19.jpg", "/profilbilder/profilbild20.jpg" ];
+
 		Accounts.createUser({
+			profile: {
+				profilbild: Random.choice(profilbilder)
+			},
 			email: emailVar,
 			password: passwordVar,
-			username: Random.id([6])
+			username: Random.choice(usernames1)+" "+Random.choice(usernames2)
 		})
 		Meteor.loginWithPassword(emailVar, passwordVar, function(){
 			if (Meteor.user()) {
@@ -59,16 +66,25 @@ Template.login.events({
 		});
 	},
 	"click #btn_register_facebook":function(event, template){
+		var usernames1 = ["grinsender", "kreischender", "schöner", "wachsamer", "großer", "weißer", "starker", "mutiger", "tanzender", "blauer", "roter", "listiger", "kräftiger", "furchtloser", "silberner", "anmutiger", "mächtiger", "mystischer", "potenter", "wohlriechender", "launischer", "lebensfroher", "geheimnisvoller", "grimmiger", "optimistischer", "pessimistischer", "respektabler", "impulsiver", "dominanter", "romantischer", "kaltblütiger", "schüchterner", "japsender", "tauchender", "rülpsender", "pfeifender", "sturer", "kitzliger", "flinker", "kleiner", "verrückter", "abgedrehter", "hyperaktiver", "trotzender", "cooler", "krasser", "schlauer"];
+		var usernames2 = ["Hummer", "Wolf", "Büffel", "Wurm", "Adler", "Stier", "Hund", "Blutegel", "Hirsch", "Pinguin", "Papagei", "Hamster", "Hase", "Nacktmul", "Zander", "Dachs", "Igel", "Blobfisch", "Flamingo", "Leopard", "Maulwurf", "Elch", "Steinbock", "Esel", "Blauwal", "Gekko", "Käfer", "Salamander", "Grizzly", "Puma", "Gepard", "Elefant", "Luchs", "Uhu", "Waschbär", "Pfau", "Fasan", "Schwan", "Rochen", "Affe", "Schimpanse", "Orang-Utan", "Kabeljau", "Aal", "Thunfisch", "Schmetterling", "Eisbär", "Tiger", "Hai", "Panda", "Mops", "Pavian", "Orka", "Dackel", "Gorilla", "Storch", "Frosch", "Krake", "Pelikan", "Koala"];
+		var profilbilder = ["/profilbilder/profilbild1.jpg", "/profilbilder/profilbild2.jpg", "/profilbilder/profilbild3.jpg", "/profilbilder/profilbild4.jpg", "/profilbilder/profilbild5.jpg", "/profilbilder/profilbild6.jpg", "/profilbilder/profilbild7.jpg", "/profilbilder/profilbild8.jpg", "/profilbilder/profilbild9.jpg", "/profilbilder/profilbild10.jpg", "/profilbilder/profilbild11.jpg", "/profilbilder/profilbild12.jpg", "/profilbilder/profilbild13.jpg", "/profilbilder/profilbild14.jpg", "/profilbilder/profilbild15.jpg", "/profilbilder/profilbild16.jpg", "/profilbilder/profilbild17.jpg", "/profilbilder/profilbild18.jpg", "/profilbilder/profilbild19.jpg", "/profilbilder/profilbild20.jpg" ];
+
 		Meteor.loginWithFacebook(function(){
-			Meteor.users.update({_id: Meteor.userId()}, {$set: {"username": Random.id([6])}} );
+			Meteor.users.update({_id: Meteor.userId()}, {$set: {"username": Random.choice(usernames1)+" "+Random.choice(usernames2)}} );
+			Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.profilbild": Random.choice(profilbilder)}} );
 			if (Meteor.user()) {
 				Router.go("chooseUserclass")
 			};
 		});
 	},
 	"click #btn_register_twitter":function(event, template){
+		var usernames1 = ["grinsender", "kreischender", "schöner", "wachsamer", "großer", "weißer", "starker", "mutiger", "tanzender", "blauer", "roter", "listiger", "kräftiger", "furchtloser", "silberner", "anmutiger", "mächtiger", "mystischer", "potenter", "wohlriechender", "launischer", "lebensfroher", "geheimnisvoller", "grimmiger", "optimistischer", "pessimistischer", "respektabler", "impulsiver", "dominanter", "romantischer", "kaltblütiger", "schüchterner", "japsender", "tauchender", "rülpsender", "pfeifender", "sturer", "kitzliger", "flinker", "kleiner", "verrückter", "abgedrehter", "hyperaktiver", "trotzender", "cooler", "krasser", "schlauer"];
+		var usernames2 = ["Hummer", "Wolf", "Büffel", "Wurm", "Adler", "Stier", "Hund", "Blutegel", "Hirsch", "Pinguin", "Papagei", "Hamster", "Hase", "Nacktmul", "Zander", "Dachs", "Igel", "Blobfisch", "Flamingo", "Leopard", "Maulwurf", "Elch", "Steinbock", "Esel", "Blauwal", "Gekko", "Käfer", "Salamander", "Grizzly", "Puma", "Gepard", "Elefant", "Luchs", "Uhu", "Waschbär", "Pfau", "Fasan", "Schwan", "Rochen", "Affe", "Schimpanse", "Orang-Utan", "Kabeljau", "Aal", "Thunfisch", "Schmetterling", "Eisbär", "Tiger", "Hai", "Panda", "Mops", "Pavian", "Orka", "Dackel", "Gorilla", "Storch", "Frosch", "Krake", "Pelikan", "Koala"];
+		var profilbilder = ["/profilbilder/profilbild1.jpg", "/profilbilder/profilbild2.jpg", "/profilbilder/profilbild3.jpg", "/profilbilder/profilbild4.jpg", "/profilbilder/profilbild5.jpg", "/profilbilder/profilbild6.jpg", "/profilbilder/profilbild7.jpg", "/profilbilder/profilbild8.jpg", "/profilbilder/profilbild9.jpg", "/profilbilder/profilbild10.jpg", "/profilbilder/profilbild11.jpg", "/profilbilder/profilbild12.jpg", "/profilbilder/profilbild13.jpg", "/profilbilder/profilbild14.jpg", "/profilbilder/profilbild15.jpg", "/profilbilder/profilbild16.jpg", "/profilbilder/profilbild17.jpg", "/profilbilder/profilbild18.jpg", "/profilbilder/profilbild19.jpg", "/profilbilder/profilbild20.jpg" ];
 		Meteor.loginWithTwitter(function(){
-			Meteor.users.update({_id: Meteor.userId()}, {$set: {"username": Random.id([6])}} );
+			Meteor.users.update({_id: Meteor.userId()}, {$set: {"username": Random.choice(usernames1)+" "+Random.choice(usernames2)}} );
+			Meteor.users.update({_id: Meteor.userId()}, {$set: {"profile.profilbild": Random.choice(profilbilder)}} );
 			if (Meteor.user()) {
 				Router.go("chooseUserclass")
 			};
