974be5f35c89454514af1deb69ca0287717ac90b
ghalia <ghalia.rihawi93@gmail.com>
2015-06-02 16:00:47 +0200

    CollectionFS(image-upload)+server mothods

--- server/server.js
+++ server/server.js
@@ -8,7 +8,22 @@ Meteor.users.allow({
 			insert: function(userId, role){
 				return true;
 			}
-})
+});
+
+Images.allow({
+ 		insert: function(){
+ 				return true;
+ 		},
+ 		update: function(){
+ 				return true;
+ 		},
+ 		remove: function(){
+ 				return true;
+ 		},
+ 		download: function(){
+ 			return true;
+ 		}
+});
 
 // server: publish all question documents
 Meteor.publish("allQuestions", function () {
@@ -18,4 +33,9 @@ Meteor.publish("allQuestions", function () {
 // server: publish all answers documents
 Meteor.publish("allAnswers", function () {
   return Answers.find(); // everything
+});
+
+// server: publish all images
+Meteor.publish("images", function() {
+	 return Images.find();
 });
