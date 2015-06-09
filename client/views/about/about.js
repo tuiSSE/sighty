d44bd84da960f79601da55ff9ebad32403ece886
Felix <felix.schroeter@tu-ilmenau.de>
2015-06-09 21:20:10 +0200

    Zu meinem Account hinzufügen
    Machs nur kurz raus und wieder rein. Hat den commit beim letzten mal
    nicht  zu meinem Account hinzugefügt, weil ich an einem anderen Pc war

---
+++ client/views/about/about.js
@@ -0,0 +1,48 @@
+Router.map(function(){
+	this.route("about", {path:"/about"})
+})
+
+Template.about.rendered=function(){
+	$('.button-collapse').sideNav({
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+}
+
+Template.about.helpers({
+	profilbild:function(){
+		return Meteor.user().profile.profilbild;
+	},
+	username:function(){
+		return Meteor.user().username;
+	},
+	version:function(){
+    return Version.findOne();
+  }
+})
+
+Template.about.events({
+	"click #btn_goto_home1":function(event, template){
+		if (Roles.userIsInRole({_id: Meteor.userId()}, "helper")) {
+			Router.go("homeHelper")
+		} else if (Roles.userIsInRole({_id: Meteor.userId()}, "blind")) {
+			Router.go("homeBlind")
+		};
+	},
+	"click #btn_goto_home2":function(event, template){
+		if (Roles.userIsInRole({_id: Meteor.userId()}, "helper")) {
+			Router.go("homeHelper")
+		} else if (Roles.userIsInRole({_id: Meteor.userId()}, "blind")) {
+			Router.go("homeBlind")
+		};
+	},
+	"click #btn_logout_about1":function(event, template){
+		Meteor.logout();
+		Router.go("login")
+	},
+	"click #btn_logout_about2":function(event, template){
+		Meteor.logout();
+		Router.go("login")
+	}
+})
