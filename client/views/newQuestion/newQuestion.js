e36419bcd5842a347cfd919a111527714d9761a3
ghalia <ghalia.rihawi93@gmail.com>
2015-06-10 16:32:28 +0200

    newQuestion-page update

--- client/views/newQuestion/newQuestion.js
+++ client/views/newQuestion/newQuestion.js
@@ -60,9 +60,12 @@ Template.newQuestion.events({
           }
         }
       );
-      Session.set('question', text);
-			Session.set('quesId', newQues._id);
+			Session.set('photo1', null);
+			Session.set('photo2', null);
+			Session.set('photo3', null);
+			Session.set('photo4', null);
 			$('#question').val('');
+			$('#btn_gobackto_homeBlind').click();
   },
   'click #deletePic1': function (event,template) {
     event.preventDefault();
