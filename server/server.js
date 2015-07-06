e879458be392f859ca4dd5e0f53c4883370dc271
guro3210 <rosenbergerguido@gmail.com>
2015-07-06 20:52:51 +0200

    edited sent email

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ Meteor.startup(function(){
 })
 Meteor.absoluteUrl("https://sighty.theoinf.tu-ilmenau.de");
 Meteor.absoluteUrl.defaultOptions.rootUrl = "https://sighty.theoinf.tu-ilmenau.de";
+Accounts.emailTemplates.siteName = "Sighty";
+Accounts.emailTemplates.from = "Sighty Team <sightyteam@gmail.com>";
+Accounts.emailTemplates.resetPassword.subject = function (user) {
+  return "So setzen Sie Ihr Sighty-Passwort zurueck"
+};
+Accounts.emailTemplates.resetPassword.text = function(user, url) {
+  url = url.replace('#/', '');
+  return "Hallo, \n klicken Sie auf diesen Link, um ihr Passwort zuruecksetzen zu koennen: \n" + url + "\n\nIhr Sighty Team";
+};
 
 Push.allow({
     send: function(userId, notification) {
